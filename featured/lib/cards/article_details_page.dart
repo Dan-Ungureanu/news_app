e100b01719206ceeb2f21db743e1fab6382a0805
Dan Ungureanu <danungureanu@EBS-PC-059.local>
2025-08-08 11:14:34 +0300

    small bug fix for bottom tags

--- featured/lib/cards/article_details_page.dart
+++ featured/lib/cards/article_details_page.dart
@@ -32,177 +32,195 @@ class _ArticleDetailsPageState extends State<ArticleDetailsPage> {
     }
 
     return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: onRefresh,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () => Get.back(),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 47.0, bottom: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.arrow_back_outlined, color: Colors.red),
-                    ],
-                  ),
-                ),
-              ),
-              Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-
-                  child: article.urlToImage != null
-                      ? Image.network(
-                          article.urlToImage!,
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: 252,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Image.asset(
-                              'images/unavailable.png',
-                              fit: BoxFit.contain,
-                              width: double.infinity,
-                              height: 252,
-                            );
-                          },
-                        )
-                      : Image.asset(
-                          'images/unavailable.png',
-                          fit: BoxFit.contain,
-                          width: double.infinity,
-                          height: 252,
-                        ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              Text(
-                article.title ?? 'No Title',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: CategoryWidget(),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Icon(Icons.remove_red_eye, color: Colors.red),
-                      ),
-                      NumberOfFavoriteWidget(),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: LikeWidget(),
-                      ),
-                      LikeNumberWidget(),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: CommentWidget(),
-                      ),
-                      NumberOfCommentWidget(),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundImage: iconCategories[number],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          textCategory[number],
-                          style: Theme.of(context).textTheme.bodySmall
-                              ?.copyWith(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                              ),
-                        ),
-                        Text(
-                          article.publishedAt != null
-                              ? _getDaysAgoText(article.publishedAt!)
-                              : '',
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodySmall?.copyWith(fontSize: 10),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Image.asset('images/follow.png'),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Text(article.description ?? ''),
-              const SizedBox(height: 10),
-              Text(article.content ?? ''),
-              const SizedBox(height: 20),
-              SafeArea(
-                bottom: true,
-                left: true,
-                right: false,
-                maintainBottomViewPadding: false,
+      body: Column(
+        children: [
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: onRefresh,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+                    GestureDetector(
+                      onTap: () => Get.back(),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 47.0, bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.arrow_back_outlined, color: Colors.red),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+
+                        child: article.urlToImage != null
+                            ? Image.network(
+                                article.urlToImage!,
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                                height: 252,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Image.asset(
+                                    'images/unavailable.png',
+                                    fit: BoxFit.contain,
+                                    width: double.infinity,
+                                    height: 252,
+                                  );
+                                },
+                              )
+                            : Image.asset(
+                                'images/unavailable.png',
+                                fit: BoxFit.contain,
+                                width: double.infinity,
+                                height: 252,
+                              ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Text(
+                      article.title ?? 'No Title',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 20.0),
+                              child: CategoryWidget(),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                              ),
+                              child: Icon(
+                                Icons.remove_red_eye,
+                                color: Colors.red,
+                              ),
+                            ),
+                            NumberOfFavoriteWidget(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                              ),
+                              child: LikeWidget(),
+                            ),
+                            LikeNumberWidget(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                              ),
+                              child: CommentWidget(),
+                            ),
+                            NumberOfCommentWidget(),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage: iconCategories[number],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                textCategory[number],
+                                style: Theme.of(context).textTheme.bodySmall
+                                    ?.copyWith(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                              ),
+                              Text(
+                                article.publishedAt != null
+                                    ? _getDaysAgoText(article.publishedAt!)
+                                    : '',
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.bodySmall?.copyWith(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Image.asset('images/follow.png'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Text(article.description ?? ''),
+                    const SizedBox(height: 10),
+                    Text(article.content ?? ''),
                     const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Image.asset('images/politics.png', width: 80),
-                        Image.asset('images/usa.png', width: 80),
-                        Image.asset('images/joebiden.png', width: 80),
-                        Image.asset('images/news.png', width: 80),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Image.asset('images/america.png', width: 80),
-                        Image.asset('images/uk.png', width: 80),
-                        Image.asset('images/politics.png', width: 80),
-                        Image.asset('images/politics.png', width: 80),
-                      ],
-                    ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+          SafeArea(
+            bottom: true,
+            left: true,
+            right: false,
+            maintainBottomViewPadding: false,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset('images/politics.png', width: 80),
+                      Image.asset('images/usa.png', width: 80),
+                      Image.asset('images/joebiden.png', width: 80),
+                      Image.asset('images/news.png', width: 80),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset('images/america.png', width: 80),
+                      Image.asset('images/uk.png', width: 80),
+                      Image.asset('images/politics.png', width: 80),
+                      Image.asset('images/politics.png', width: 80),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
